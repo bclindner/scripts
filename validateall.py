eff2d803b04ad04321a0010358cf6e3cff96f07c
Brian C. Lindner <brian@bclindner.com>
2018-02-28 09:27:57 -0500

    improvements to validateall: OOP & CSS validation
    Classed out the validation system and added a
    hacky-but-semi-competent CSS validator, using
    the WSDL service on jigsaw.w3.org for CSS
    validation.

--- validateall.py
+++ validateall.py
@@ -2,35 +2,80 @@
 
 ## Validate all HTML files through Validator.nu.
 
-import urllib.request as url
-import json
-import os
+import urllib.request, urllib, json, os, re
+import xml.etree.ElementTree as XML
+class ValidationMessage:
+    def __init__(self, line, message):
+        self.line = line
+        self.msg = message
+    def __str__(self):
+        return f'On line {self.line}: {self.msg}'
+class HTMLValidator:
+
+    """ Validate HTML using the Validator.nu Web service.
+
+    Keyword arguments:
+    code -- a bytes-like object in HTML
+
+    """
+
+    url = 'http://html5.validator.nu?out=json'
+    def __init__(self, code):
+        self.code = code
+    def __enter__(self):
+        self.errors = []
+        req = urllib.request.Request(self.url)
+        req.add_header('Content-Type','text/html')
+        req.data = self.code.read().encode('utf-8')
+        with urllib.request.urlopen(req) as res:
+            msgs = json.load(res)['messages']
+            for msg in msgs:
+                if msg['type'] == "error":
+                    self.errors.append(ValidationMessage(msg['lastLine'],msg['message']))
+        return self.errors
+    def __exit__(self, errtype, errval, trace):
+        return
+class CSSValidator:
+    url = 'https://jigsaw.w3.org/css-validator/validator?'
+    def __init__(self, code):
+        self.code = code
+    def __enter__(self):
+        self.errors = []
+        data = {
+                "text": self.code.read(),
+                "output": "soap12",
+                "profile": "css3"
+                }
+        req = urllib.request.Request(self.url+urllib.parse.urlencode(data))
+        with urllib.request.urlopen(req) as res:
+            soap = XML.fromstring(res.read())
+            for errmsg in soap.iter(tag="{http://www.w3.org/2005/07/css-validator}error"):
+                line = errmsg.find('{http://www.w3.org/2005/07/css-validator}line').text.strip()
+                msg = errmsg.find('{http://www.w3.org/2005/07/css-validator}message').text.strip()
+                msg = re.sub(r'\s+', ' ', msg)
+                self.errors.append(ValidationMessage(line, msg))
+        return self.errors
+    def __exit__(self, errtype, errval, traceback):
+        return
 
 # pwd
 dir = os.listdir('.')
 # check if any files are html
 for listing in dir:
-    if listing[-4:] == "html":
-        # open valid files
+    if listing[-3:] == "css":
+        print(listing+":")
+        pass
+        # open valid html file
         with open(listing) as file:
-            print('[INFO] found file '+listing+'. validating...')
-            # send them to validator.nu
-            req = url.Request('http://html5.validator.nu?out=json')
-            req.add_header('Content-Type','text/html')
-            req.data = file
-            with url.urlopen(req) as res:
-                # set flag for errors
-                errors = False
-                # get messages and parse them
-                msgs = json.load(res)['messages']
-                for msg in msgs:
-                    # if there are errors, output them
-                    if msg['type'] == "error":
-                        # set errored flag
-                        errors = True
-                        print('[ERR!] on line '+str(msg['lastLine'])+", column "+str(msg['lastColumn'])+": "+msg['message'])
-                # say if there were errors or not based on flag
-                if errors:
-                    print("[ERR!] there were errors")
-                else:
-                    print("[INFO] no errors found")
+            # run through HTMLValidator
+            with CSSValidator(file) as errors:
+                for error in errors:
+                    print(error)
+    if listing[-4:] == "html":
+        print(listing+":")
+        # open valid html file
+        with open(listing) as file:
+            # run through HTMLValidator
+            with HTMLValidator(file) as errors:
+                for error in errors:
+                    print(error)
