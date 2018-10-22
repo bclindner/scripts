2de5f9a56ada33e785eaea08b3334994be00cc59
Brian C. Lindner <brian@bclindner.com>
2018-10-22 00:43:47 -0400

    added fortnite-dab-watch.py

---
+++ fortnite-dab-watch.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+# fortnite-dab-watch.py:
+# Description: Script that checks for dab emotes in the Fortnite store and prints them.
+# Usage: launch script daily after Fortnite store refresh
+# Requires: Python 3, Requests lib, my notifs library, default Python libs
+#
+# dev notes: why did i do this
+import requests
+from notifs import sendMessage
+import json
+
+with open('fortnite-dab-watch.json') as file:
+    config = json.load(file)
+
+resp = requests.get('https://api.fortnitetracker.com/v1/store', headers={'TRN-Api-Key': config['apiKey']})
+storeItems = resp.json()
+dabItems = [item['name'] for item in storeItems if 'dab' in item['name'].lower()]
+if len(dabItems) > 0:
+    sendMessage("Dab items available in the Fortnite store today:\nItems on offer:\n"+'\n'.join(dabItems),config['notifs'])
+
