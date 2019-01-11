c4ad37db88c4a8acacf6f16529fc4a04bd75e5e2
Brian C. Lindner <brian@bclindner.com>
2019-01-10 22:01:47 -0500

    updated textemoji.py

--- textemoji.py
+++ textemoji.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 
 # textemoji.py:
-# Description: Converts a string into AlTeRnAtInG cASe and copies it to the clipboard.
+# Description: Converts a string into emoji shortcodes and copies it to the clipboard.
 # Usage: ./textemoji.py [prefix] "[text to convert to text-emoji]" [suffix]
 # Requires: Python 3 and pyperclip library
 # dev notes: this is designed primarily to generate text from emoji shortcodes, in particular Discord's :regional_indicator_*: emojis.
 # it's in this repo just so i have it on hand when i need it on my machines.
-import sys, pyperclip
+import sys, pyperclip, pyautogui
 
 def textemoji(prefix, string, suffix):
     ret = ""
